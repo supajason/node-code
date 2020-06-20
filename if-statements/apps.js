84f4b340522923251c19176002efc704048e6d7e
Jason Edmonds <jasonedmonds1@gmail.com>
2020-06-20 13:33:35 +0100

    Added statements

--- if-statements/apps.js
+++ if-statements/apps.js
@@ -1,3 +1,13 @@
 // if statements
-
-//testing new SSH key
+console.log(`1 == \`1\``); 
+if (1 == `1`) {
+    console.log(`1 does equal 1`); 
+} else {
+    console.log(`1 doesn't equal 1`);
+}
+console.log(`1 === \`1\``); 
+if (1 === `1`) {
+    console.log(`1 does equal 1`); 
+} else {
+    console.log(`1 doesn't equal 1`);
+}
